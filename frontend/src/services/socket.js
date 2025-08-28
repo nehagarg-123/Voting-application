1fc89cdf106a59bedb289e38107cfb359609f3e3
nehagarg-123 <nehagarg6201@gmail.com>
2025-08-28 15:12:46 +0530

    Update socket.js

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // Connect to backend
-export const socket = io("https://voting-application-5wm0.onrender.com", {
+export const socket = io(import.meta.env.VITE_API_BASE_URL, {
   transports: ["websocket"] // Ensures stable connection
 });
