cb298eef5f1345129900453adda08c56e199fd4a
nehagarg-123 <nehagarg6201@gmail.com>
2025-08-28 16:05:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,18 @@ const db = require('./db'); // MongoDB connection
 const Candidate = require('./models/candidate'); // Only needed for results
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173","https://voting-application-frontend.onrender.com", methods: ["GET", "POST"] }));
+//app.use(cors({ origin: "https://voting-application-frontend.onrender.com", methods: ["GET", "POST"] }));
+app.use(cors({
+  origin: ["https://voting-application-frontend.onrender.com"], // add your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(bodyParser.json());
 
 // HTTP + Socket.io setup
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: { origin: "http://localhost:5173","https://voting-application-frontend.onrender.com", methods: ["GET", "POST"] }
+    cors: { origin: "https://voting-application-frontend.onrender.com", methods: ["GET", "POST"] }
 });
 app.set('socketio', io);
 
