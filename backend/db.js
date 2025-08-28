d55766bd56b2011c8d6188a1fb3e5b4cc3fb6b60
nehagarg-123 <nehagarg6201@gmail.com>
2025-08-28 10:10:28 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 
 // Define the MongoDB connection URL
-const mongoURL = process.env.MONGODB_URL || process.env.MONGODB_URL_LOCAL; // Replace 'mydatabase' with your database name
+const mongoURL = process.env.MONGODB_URL_LOCAL; // Replace 'mydatabase' with your database name
 // const mongoURL = process.env.MONGODB_URL;
 
 // Set up MongoDB connection
